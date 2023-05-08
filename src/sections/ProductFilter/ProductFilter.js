e85378dca5e94a41fd374d97ed12958e5514a51a
naima <cours.dev60@gmail.com>
2023-05-08 15:38:53 +0200

    style boutons catagory

--- src/sections/ProductFilter/ProductFilter.js
+++ src/sections/ProductFilter/ProductFilter.js
@@ -37,35 +37,33 @@ const ProductFilter = () => {
             <Api url='http://127.0.0.1:8000/api/category/' render={(categories) => (  
               <>
                 {categories.map(category => (
-                  <h1 key={category.id}>
-                    <button className='btn btn-color-all w-100 mb-4'
+                    <button className='btn btn-category w-100 mb-4' key={category.id}
                      onClick={() => handleCategoryClick(category.id)}>{category.libelle}
-                     </button>
-                  </h1>
+                     </button>                  
                 ))}
               </>
             )}/>
           </div>
           </div>
           <Api url={getProductListUrlByCategoryId(selectedCategoryId)} render={(products) => (
-            <div className='col-md-10'>
-              <div className='row'>
-                {products.map(produit => (
-                  <div className='col-md-2 mb-4 card-container' key={produit.id}>
-                    <div className="card" >
-                      <img src={produit.image} className="card-img-top" alt={produit.libelle} />
-                      <div className="card-body">
-                        <h5 className="card-title">{produit.libelle}</h5>
-                        <p className="card-text">{produit.description}</p>
-                        <h4 className="card-price">
-                        <span style={{ color: produit.remise > 0 ? "red" : "inherit" }}>                          
-                          {produit.remise >0 ? produit.remise : produit.prix} €
-                          </span>
-                          </h4>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+                  <div className='col-md-10'>
+                    <div className='row'>
+                      {products.map(produit => (
+                        <div className='col-md-2 mb-4 card-container' key={produit.id}>
+                          <div className="card" >
+                            <img src={produit.image} className="card-img-top" alt={produit.libelle} />
+                            <div className="card-body">
+                              <h5 className="card-title">{produit.libelle}</h5>
+                              <p className="card-text">{produit.description}</p>
+                              <h4 className="card-price">
+                              <span style={{ color: produit.remise > 0 ? "red" : "inherit" }}>                          
+                                {produit.remise >0 ? produit.remise : produit.prix} €
+                                </span>
+                                </h4>
+                            </div>
+                          </div>
+                        </div>
+                   ))}
               </div>
             </div>
           )}/>
